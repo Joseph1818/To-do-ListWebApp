3d0d96a5fa711b93dc49d12d24ecf1257ac1ca4e
Joseph1818 <ujee@UMBAS-MacBook-Air.local>
2023-08-01 10:46:34 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
